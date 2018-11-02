628ae5a2d0e03f312cfcb0ccf612550a245dfeb0
László Major <laszlo.major@outlook.com>
2018-11-02 20:51:45 +0100

    Fix webkitdirectory error

--- wharf-client/src/renderer/components/BootstrapForm.tsx
+++ wharf-client/src/renderer/components/BootstrapForm.tsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { createRef } from "react";
 import { connect } from "react-redux";
 
 import * as IpcHandler from "../IpcHandler";
 import Frame from "./Frame";
 
-const BootstrapForm = () => {
-    let localRepoRoot: any;
-    let serverConfigUrl: HTMLInputElement | null;
+class BootstrapForm extends React.Component<{}, {}> {
+    private localRepoRoot = createRef<HTMLInputElement>();
+    private serverConfigUrl = createRef<HTMLInputElement>();
 
-    function onClick() {
+    componentDidMount() {
+        (this.localRepoRoot.current as any).webkitdirectory = true;
+    }
+
+    bootstrapOnLick() {
+        const localRepoRoot: any = this.localRepoRoot.current;
+        const serverConfigUrl = this.serverConfigUrl.current;
         if (!localRepoRoot || !localRepoRoot.value) {
             return;
         }
@@ -18,23 +24,25 @@ const BootstrapForm = () => {
         IpcHandler.bootrapConfig(localRepoRoot.files[0].path.trim(), serverConfigUrl.value.trim());
     }
 
-    return <Frame>
-        <div className="field">
-            <label className="label" htmlFor="local-repo-root">Repo folder</label>
-            <input id="local-repo-root" name="local-repo-root" className="input" type="file" webkitdirectory="true"
-                ref={node => { localRepoRoot = node; }} />
-        </div>
-        <div className="field">
-            <label className="label" htmlFor="server-config-url">Repo URL</label>
-            <input id="server-config-url" name="server-config-url" className="input" type="text"
-                defaultValue="https://ark-group.org/wharf/config.json"
-                ref={node => { serverConfigUrl = node; }} />
-        </div>
+    render() {
+        return <Frame>
+            <div className="field">
+                <label className="label" htmlFor="local-repo-root">Repo folder</label>
+                <input id="local-repo-root" name="local-repo-root" className="input" type="file"
+                    ref={this.localRepoRoot} />
+            </div>
+            <div className="field">
+                <label className="label" htmlFor="server-config-url">Repo URL</label>
+                <input id="server-config-url" name="server-config-url" className="input" type="text"
+                    defaultValue="https://ark-group.org/wharf/config.json"
+                    ref={this.serverConfigUrl} />
+            </div>
 
-        <div className="control">
-            <button id="bootstrap-button" className="button is-primary" onClick={onClick}>Setup</button>
-        </div>
-    </Frame>;
-};
+            <div className="control">
+                <button id="bootstrap-button" className="button is-primary" onClick={() => this.bootstrapOnLick()}>Setup</button>
+            </div>
+        </Frame>;
+    }
+}
 
 export default connect()(BootstrapForm);
